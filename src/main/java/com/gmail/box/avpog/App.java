47ab7343ba4889ce8ce4a4c4ce028a6907146dfe
Poharskyi <avpog.box@gmail.com>
2024-11-12 20:29:03 +0100

    first commit

---
+++ src/main/java/com/gmail/box/avpog/App.java
@@ -0,0 +1,13 @@
+package com.gmail.box.avpog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
