6b5fa858336e07962fa1bb03a0cab0af6ccdd23b
Poharskyi <avpog.box@gmail.com>
2024-11-13 00:42:27 +0100

    done code v2

--- src/main/java/com/gmail/box/avpog/Utils/Util.java
+++ src/main/java/com/gmail/box/avpog/Utils/Util.java
@@ -1,4 +1,12 @@
 package com.gmail.box.avpog.Utils;
 
 public class Util {
+
+    public static void arrIntPrint(int[] arr) {
+        int arrLength = arr.length;
+        for (int i = 0; i < arrLength; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
